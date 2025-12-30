785251093d5207628fbc09b2ba60e2c703fd4ff5
Teodor <stevanovicteodor002@gmail.com>
2025-12-30 08:01:45 +0100

    rendered tasks list from state

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,69 @@
 import { useState } from "react";
-import { Button, Form, Input, Layout, Modal } from "antd";
+import { Button, Form, Input, Layout, message, Modal, List } from "antd";
 import { Content, Header } from "antd/es/layout/layout";
 import TextArea from "antd/es/input/TextArea";
 
 function App() {
+  const [form] = Form.useForm();
   const [tasks, setTasks] = useState([]);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const showModal = () => {
     setIsModalOpen(true);
   };
   const handleOk = () => {
-    setIsModalOpen(false);
+    form
+      .validateFields()
+      .then((values) => {
+        setTasks((prev) => [...prev, values]);
+        setIsModalOpen(false);
+        form.resetFields();
+      })
+      .catch(() => {});
   };
   const cancleModal = () => {
     setIsModalOpen(false);
   };
   return (
-    <>
+    <main>
       <Layout style={{ minHeight: "100vh" }}>
-        <Header style={{ color: "#fff", padding: "0 20px" }}>Header</Header>
+        <Header style={{ color: "#fff", padding: "0 20px" }}>To Do App</Header>
         <Layout>
           <Content style={{ padding: "20px" }}>
             <Button type="primary" onClick={showModal}>
               Add Task
             </Button>
+            <List
+              dataSource={tasks}
+              locale={{ emptyText: "No tasks yet" }}
+              renderItem={(item, index) => (
+                <List.Item key={index}>
+                  <strong>{item.name}</strong> {item.task}
+                </List.Item>
+              )}
+            ></List>
             <Modal
               title="Add Task"
               open={isModalOpen}
               onOk={handleOk}
               onCancel={cancleModal}
             >
-              <Form>
-                <Form.Item label="Name" name="name">
-                  <Input/>
+              <Form form={form}>
+                <Form.Item
+                  label="Name"
+                  name="name"
+                  rules={[{ required: true, message: "Input your name." }]}
+                >
+                  <Input />
                 </Form.Item>
                 <Form.Item label="Task" name="task">
-                  <TextArea rows={20}/>
+                  <TextArea rows={20} />
                 </Form.Item>
               </Form>
             </Modal>
           </Content>
         </Layout>
       </Layout>
-    </>
+    </main>
   );
 }
 
